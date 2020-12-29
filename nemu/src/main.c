931bcb4e4518ac2459df9152a97863f95ac968c5
tracer-ics2019 <tracer@njuics.org>
2020-12-29 10:48:40 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:48:40 up 57 min,  1 user,  load average: 0.55, 0.36, 0.36 b4e962804fadd3a95cf00638ad158e688f08cc84

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,12 +1,16 @@
+
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
-  int is_batch_mode = init_monitor(argc, argv);
+  //int is_batch_mode = init_monitor(argc, argv);
 
   /* Receive commands from user. */
-  ui_mainloop(is_batch_mode);
-
+  //ui_mainloop(is_batch_mode);
+  char *a;
+  char ch[] = "392-583*170";
+  printf("%d\n", expr(ch, a));
+  printf("%d\n", *a);
   return 0;
 }
--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -5,11 +5,50 @@
 #include <assert.h>
 #include <string.h>
 
+#define NR_TOKEN 500
+#define MAXNUM 1000
+
 // this should be enough
-static char buf[65536];
+static char buf[60000];
+int len, nr;
+
+static uint32_t choose(uint32_t n){
+	return rand() % n;	
+}
+
+static void gen_num(){
+	if(nr >= NR_TOKEN) return ;
+	int x = choose(MAXNUM) + 1;
+	len += sprintf(buf + len, "%d", x);
+	++ nr;
+}
+
+static void gen(char ch){
+	if(nr >= NR_TOKEN && ch != ')') return ;
+	buf[len ++] = ch; 
+	++ nr;
+}
+
+static void gen_rand_op(){
+	if(nr >= NR_TOKEN) return ;
+	static char ops[] = "+-*/";
+	buf[len ++] = ops[choose(4)];
+	++ nr;
+}
+
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
-  
+	if(nr >= NR_TOKEN) return ;
+	switch(choose(3)){
+		case 0:	gen_num(); break;
+		case 1:
+			if(nr + 3 > NR_TOKEN) return ;	
+			gen('('); gen_rand_expr(); gen(')');
+			break;
+		default:
+			if(nr + 3 > NR_TOKEN) return ;	
+			gen_rand_expr(); gen_rand_op(); gen_rand_expr();
+			break;
+	}
 }
 
 static char code_buf[65536];
@@ -30,7 +69,10 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
+    len = 0;
+    nr = 0;
     gen_rand_expr();
+    buf[len] = '\0';
 
     sprintf(code_buf, code_format, buf);
 
@@ -50,6 +92,8 @@ int main(int argc, char *argv[]) {
     pclose(fp);
 
     printf("%u %s\n", result, buf);
+
+    memset(buf, '\0', 60000);
   }
   return 0;
 }
