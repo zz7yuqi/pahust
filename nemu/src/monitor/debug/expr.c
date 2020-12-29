2d4d11bdec7c16f5c6e12f19dc62cc6d4096393e
tracer-ics2019 <tracer@njuics.org>
2020-12-29 12:12:47 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:12:47 up  2:21,  1 user,  load average: 0.40, 0.62, 0.68 ff75f24ccf55bd47edcf2fd4df8bf5b0646f49c0

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -105,9 +105,11 @@ static bool make_token(char *e) {
           case TK_HEX:
 			    case TK_REG:
           case TK_NUM:
-                        tokens[nr_token++].type = rules[i].token_type;
+                        tokens[nr_token].type = rules[i].token_type;
                         strncpy(tokens[nr_token].str, substr_start, substr_len);
 				                tokens[nr_token].str[substr_len] = '\0';
+                        printf("%s\n", tokens[nr_token].str);
+                        nr_token++;
                         break;
           
 
@@ -301,7 +303,6 @@ uint32_t expr(char *e, bool *success) {
     *success = false;
     return 0;
   }
-  for (int i = 0; i < nr_token; i++) printf("%s\n", tokens[i].type);
 
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
