b58fde515d3752ef9b1e4c827f0d62e9b2e28c9c
tracer-ics2019 <tracer@njuics.org>
2020-12-29 12:06:59 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:06:59 up  2:16,  1 user,  load average: 0.72, 0.75, 0.71 d47f69183b5e38894386bcaccd1b9ea0625abcaf

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -1,16 +1,44 @@
-
+#include "monitor/expr.h"
 int init_monitor(int, char *[]);
 void ui_mainloop(int);
+
+void test_expr()
+{
+	FILE *fp = fopen("tools/gen-expr/input", "r");
+	if(fp == NULL) {
+		Log("File: %s open failed\n", "tools/gen-expr/input");
+		return ;
+	}
+	Log("Testing the expr eval...\n");
+
+	uint32_t std_ret, my_ret;
+	bool success;
+	char Expr[6000];
+	bool pass = true;
+	while(fscanf(fp, "%u %s", &std_ret, Expr) != EOF)
+	{
+		success = true;
+		my_ret = expr(Expr, &success);
+		if(!success){
+			pass = false;
+			printf("expr test failed at: %s\n", Expr);
+			printf("std result: %u, my result %u\n", std_ret, my_ret);
+		}else{
+			printf("test passed at: %s\n", Expr);
+			printf("std result: %u, my result %u\n", std_ret, my_ret);
+		}
+	}
+	
+	if(pass) {
+		Log("Congratulations! You have passed all the test cases.\n");
+	}
+}
 
 int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
-  char *a;
-  char ch[] = "392+583*170";
-  unsigned int res = expr(ch, a);
-  printf("%d\n", res);
-  printf("%d\n", *a);
+  test_expr();
 
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
