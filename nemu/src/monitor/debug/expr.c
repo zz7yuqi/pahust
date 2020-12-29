adb4c568b303991110096fec59fc145f1d2fb7a3
tracer-ics2019 <tracer@njuics.org>
2020-12-29 12:24:10 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:24:10 up  2:33,  1 user,  load average: 0.60, 0.39, 0.49 a87670f27878e93991302f86822805d7b8a1ca1c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -309,7 +309,7 @@ uint32_t expr(char *e, bool *success) {
   }
   /* TODO: Insert codes to evaluate the expression. */
   //TODO();
-  *success = true;
+  //*success = true;
   return eval(0, nr_token - 1, success);
 
 }
