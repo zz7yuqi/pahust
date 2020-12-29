78c88370ea889fdf05c8768440719ea65ef33a11
tracer-ics2019 <tracer@njuics.org>
2020-12-29 12:47:07 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:47:07 up  2:56,  1 user,  load average: 0.34, 0.35, 0.36 b7fa3fb1818c0986c94802f069b15e5e5a51811c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -281,9 +281,8 @@ uint32_t eval(int p, int q, bool *legal) {
 
   Log("Main op is found.op is %d.It's type is %d.", op, tokens[op].type);
   // Main op is found.
-  int val1 = eval(p, op - 1, legal);
-  Log("val1 is %d.", val1);
-  int val2 = eval(op + 1, q, legal);
+  uint32_t val1 = eval(p, op - 1, legal);
+  uint32_t val2 = eval(op + 1, q, legal);
 
   if (*legal == false) return -1;
 
