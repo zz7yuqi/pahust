1a10e478032fb98c937ac1e6254f209ed4ab6746
tracer-ics2019 <tracer@njuics.org>
2021-01-06 17:04:39 +0800

    >  run U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:04:39 up  7:47,  1 user,  load average: 0.83, 1.12, 0.96 36c92c59a44b118e48ae3c49af92388e793c98e6

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,7 +4,56 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  return 0;
+  va_list ap;
+  va_start(ap, fmt);
+
+  uint8_t flag = 0;
+  int i = 0, j = 0, k, res = 0;
+  int tempInt;
+  char tempStr[32];
+  char *tstr;
+  while (*(fmt + i) != '\0')
+  {
+    if (!flag && *(fmt + i) != '%') {
+      _putc(*(fmt + i));
+      j++;
+    }
+    else if (!flag && *(fmt + i) == '%') flag = 1;
+    else if (flag) {
+      switch(*(fmt + i)) {
+        case 'd' :
+          res++;      
+          tempInt = va_arg(ap, int);
+          i2a(tempInt, tempStr);
+          k = 0;
+          while(tempStr[k] != '\0'){
+            _putc(tempStr[k]);
+            j++;k++;
+          }
+          flag = 0;
+          break;
+        case 's' :
+          res++;
+          flag = 0;
+          tstr = va_arg(ap, char *);
+          k = 0;
+          while(tstr[k] != '\0'){
+            _putc(tstr[k]);
+            j++;k++;
+          }
+
+          break;
+        default:
+          res = -1;
+          break;
+      }
+    }
+    i++;
+  }
+
+
+  va_end(ap);
+  return res;
 }
 
 void i2a(int n, char *dst) {
