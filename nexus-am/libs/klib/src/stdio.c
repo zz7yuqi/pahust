3dd30b3b1d95c871df1448944b57d0b8a120639c
tracer-ics2019 <tracer@njuics.org>
2021-01-06 16:36:28 +0800

    >  run U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:36:28 up  7:19,  1 user,  load average: 0.59, 0.44, 0.39 f5feac8314ba1c869cde639bb90d77ef6f7c94e0

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -7,13 +7,84 @@ int printf(const char *fmt, ...) {
   return 0;
 }
 
+void i2a(int n, char *dst) {
+  int start = 0, total = 0, temp = n;
+  int i;
+
+  if (n < 0) {
+    start = 1;
+    dst[0] = '-';
+    n = 0 - n;
+  }
+
+  while(temp != 0) {
+    temp /= 10;
+    total++;
+  }
+  
+  temp = n;
+  *(dst + start + total) = '\0';
+  for (i = total - 1; i >= 0; i--) {
+    *(dst + start + i) = temp % 10 + '0';
+    
+    temp /= 10;
+  }
+  
+}
+
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  uint8_t flag = 0;
+  int i = 0, j = 0, k, res = 0;
+  int tempInt;
+  char tempStr[32];
+  char *tstr;
+  while (*(fmt + i) != '\0')
+  {
+    if (!flag && *(fmt + i) != '%') {
+      *(out + j) = *(fmt + i);
+      j++;
+    }
+    else if (!flag && *(fmt + i) == '%') flag = 1;
+    else if (flag) {
+      switch(*(fmt + i)) {
+        case 'd' :
+          res++;      
+          tempInt = va_arg(ap, int);
+          i2a(tempInt, tempStr);
+          k = 0;
+          while((*(out + j) = tempStr[k]) != '\0'){
+            j++;k++;
+          }
+          flag = 0;
+          break;
+        case 's' :
+          res++;
+          flag = 0;
+          tstr = va_arg(ap, char *);
+          k = 0;
+          while((*(out + j) = tstr[k]) != '\0'){
+            j++;k++;
+          }
+
+          break;
+        default:
+          res = -1;
+          break;
+      }
+    }
+    i++;
+  }
+  *(out + j) = '\0';
+  
+  return res;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  _putc('!');
-  return 0;
+  va_list va;
+  va_start(va, fmt);
+  int res = vsprintf(out, fmt, va);
+  va_end(va);
+  return res;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
