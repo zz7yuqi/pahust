58f0ca2167b237e9bab79d172a16558a68dcbe84
tracer-ics2019 <tracer@njuics.org>
2021-01-09 11:13:45 +0800

    >  run U201714565 zz7 Linux zz7c 5.4.0-59-generic #65~18.04.1-Ubuntu SMP Mon Dec 14 15:59:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:13:45 up  1:23,  1 user,  load average: 0.36, 0.45, 0.42 531f4541d2531596d732aab1a6f37f2737aaa72f

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -3,6 +3,9 @@
 #include <nemu.h>
 #include <klib.h>
 
+#define W 400
+#define H 300
+
 static inline int min(int x, int y) {
   return (x < y) ? x : y;
 }
@@ -10,12 +13,10 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      uint32_t screen_info = inl(SCREEN_ADDR);
+			info->width = W;
+			info->height = H;
+			return sizeof(_DEV_VIDEO_INFO_t);
 
-      info->width = screen_info >> 16;
-      info->height = screen_info & 0x0000ffff;
-      
-      return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
   return 0;
@@ -28,8 +29,6 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 
       int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
-      int W = screen_width();
-      int H = screen_height();
       int cp_bytes = sizeof(uint32_t) * min(w, W - x);
       uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
       for (int j = 0; j < h && y + j < H; j++){
