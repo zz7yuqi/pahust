67eee950e0ffc0a90467bacf62aea241a0a91872
tracer-ics2019 <tracer@njuics.org>
2021-01-11 14:20:23 +0800

    >  run U201714565 zz7 Linux zz7c 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:20:23 up  4:30,  1 user,  load average: 0.32, 0.38, 0.42 efaa2dd56f6b2a3492ec4ac36aaa516b6a2e2992

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,13 +2,13 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-      // case _EVENT_YIELD:
-      //     printf("Self trap!\n");
-      //     break;
-      // case _EVENT_SYSCALL:
-      //     //printf("Syscall!\n");
-      //     //do_syscall(c);
-      //     break;
+    case _EVENT_YIELD:
+          printf("Self trap!\n");
+          break;
+    case _EVENT_SYSCALL:
+          //printf("Syscall!\n");
+          //do_syscall(c);
+          break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -5,7 +5,6 @@
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 _Context* __am_irq_handle(_Context *c) {
-  printf("!!!\n");
   _Context *next = c;
   if (user_handler) {
     _Event ev = {0};
