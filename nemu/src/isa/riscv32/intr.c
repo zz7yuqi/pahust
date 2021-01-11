2bace3c997ffdc2c7db2010abd7bac013931118e
tracer-ics2019 <tracer@njuics.org>
2021-01-11 14:22:33 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:22:33 up  4:33,  1 user,  load average: 0.40, 0.36, 0.41 f720d785a4d443cb2bc10e24e74434fca780b9

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -10,7 +10,7 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
   //printf("epc%x\n", epc);
   cpu.csr[SCAUSE] = NO;
   //decinfo.jmp_pc = decinfo.isa.stvec;
-  //printf("stvec%x\n", cpu.csr[STVEC]);
+  printf("stvec%x\n", cpu.csr[STVEC]);
   decinfo.jmp_pc = cpu.csr[STVEC];
   decinfo_set_jmp(true);
 }
