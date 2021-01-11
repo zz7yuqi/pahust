8badacf042a8c90d70bb836bc0b4039b2d4ebc39
tracer-ics2019 <tracer@njuics.org>
2021-01-11 22:43:29 +0800

    >  run U201714565 zz7 Linux zz7c 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  22:43:29 up  5:26,  1 user,  load average: 0.69, 0.55, 0.53 b49e9c8d86db14b4648d086bf8f03166ba0a279d

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -27,11 +27,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
+
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, 0, invalid_read, invalid_write},
-  {"stdout", 0, 0, 0, invalid_read, invalid_write},
-  {"stderr", 0, 0, 0, invalid_read, invalid_write},
+  {"stdout", 0, 0, 0, invalid_read, ramdisk_write},
+  {"stderr", 0, 0, 0, invalid_read, ramdisk_write},
 #include "files.h"
 };
 
@@ -72,7 +73,6 @@ size_t fs_write(int fd, const void *buf, size_t len){
             len = 0;
     }
     if(!file_table[fd].write){
-        printf("%d   %d\n", file_table[fd].disk_offset, file_table[fd].open_offset);
         ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
     }
     else{
