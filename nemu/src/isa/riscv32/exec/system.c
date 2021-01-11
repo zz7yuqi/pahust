d8e6be04362f91a4e6d0a55e6386dbdd012f10dd
tracer-ics2019 <tracer@njuics.org>
2021-01-11 13:38:10 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  13:38:10 up  3:48,  1 user,  load average: 0.34, 0.42, 0.38 2797f6b6e5236af3c9fd66d2793ac36144ca8c0e

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -56,7 +56,7 @@ make_EHelper(sys){
         // csrrw
         case 0b001:
             s0 = readcsr(instr.csr);
-            printf("s0:%x\n",id_src->val);
+            printf("s0:%x\n",id_src->reg);
             writecsr(instr.csr, id_src->val);
             rtl_sr(id_dest->reg, &s0, 4);
             break;
