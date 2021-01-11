b85fab2551e6c2711587884aa622f7533fc24360
tracer-ics2019 <tracer@njuics.org>
2021-01-11 14:14:17 +0800

    >  compile U201714565 zz7 Linux zz7c 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:14:17 up  4:24,  1 user,  load average: 0.24, 0.38, 0.44 4c832bb065345b591717e27e83f27def2a6a990e

--- nemu/src/isa/riscv32/exec/system.c
+++ nemu/src/isa/riscv32/exec/system.c
@@ -43,6 +43,7 @@ make_EHelper(sys){
         //ecall
         case 0b0:
             if((instr.val & ~(0x7f))==0){
+                printf("0x%x\n", cpu.csr[STVEC]);
                 raise_intr(reg_l(17), decinfo.seq_pc-4);
             }
             else if(instr.val == 0x10200073){
