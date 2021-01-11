ffe126aab84a0e8c51ea2d9205bf10befa004306
tracer-ics2019 <tracer@njuics.org>
2021-01-11 21:16:14 +0800

    >  run U201714565 zz7 Linux zz7c 5.4.0-60-generic #67~18.04.1-Ubuntu SMP Tue Jan 5 22:01:05 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  21:16:14 up  3:59,  1 user,  load average: 0.54, 0.62, 0.60 dc4e497a4cf90d3ede80163ca58474bdaa71ea

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,6 +9,10 @@ _Context* do_syscall(_Context *c) {
   a[3] = c->GPR4;
 
   switch (a[0]) {
+    case SYS_exit:
+          naive_uload(NULL,"/bin/init");
+          _halt(a[1]);
+          break;
     case SYS_yield:
           _yield();
           c->GPRx = 0;
